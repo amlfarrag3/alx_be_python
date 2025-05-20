699065c2b0bb75c4b3ddbc5e4a1e62f51cea9432
amlfarrag3 <farragaml983@gmail.com>
2025-05-20 12:09:46 +0300

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum=number1+number2
+sub=number1-number2
+multi=number1*number2
+print ("summing of [number1] and [number2] is [sum]") 
+print ("subtraction of [number2] from [number2] is [sub]") 
+print ("multiplication of [number1] and [number2] is [multi]") 
