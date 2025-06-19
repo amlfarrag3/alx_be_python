992f798fe02b054b8bc055437a4e9cae32cfe6be
amlfarrag3 <farragaml983@gmail.com>
2025-06-19 13:02:04 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,8 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation=="add":
        result=num1+num2
     elif operation=="subtract" :
-       result=abs(num1-num2)
+       result=num1-num2
     elif operation=='multiply':
        result=num1*num2
     elif operation=='divide':
