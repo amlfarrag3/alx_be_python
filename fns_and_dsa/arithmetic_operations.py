b386d00be05fe37ec3dbea84f534577862fd2fd8
amlfarrag3 <farragaml983@gmail.com>
2025-06-19 20:11:19 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation=='multiply':
        result=num1*num2
     elif operation=='divide':
-       if num2==0:
-          print('the second number can not be zero enter avalid number: ')
-          num2=float(input('enter the valid number:'))  
-          result=num1/num2
-       result=num1/num2
+       if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
     return result
